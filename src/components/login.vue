3d7ce4c6df0f5284a4d7351c4b77ab3db9c60694
osennyaya <supersduby@gmail.com>
2021-04-16 11:38:33 +0800

    add login window

---
+++ src/components/login.vue
@@ -0,0 +1,103 @@
+<template>
+  <form @submit.prevent="login" v-if="!user && islogin" class="login">
+    <div>
+      <label for="username">Username: </label>
+      <input v-model="username" type="text" id="username" name="username" required>
+    </div>
+    <div>
+      <label for="pass">Pass: </label>
+      <input v-model="password" type="password" id="pass" name="pass" required>
+    </div>
+    <div class="buttons">
+      <input type="submit" value="submit">
+      <input @click="islogin=false" type="button" value="register">
+    </div>
+  </form> 
+  <form @submit.prevent="register" v-if="!user && !islogin" class="register">
+    <div>
+      <label for="username">Username: </label>
+      <input v-model="username" type="text" id="username" name="username" required>
+    </div>
+    <div>
+      <label for="pass">Pass: </label>
+      <input v-model="password" @input="validateConfirm" type="password" id="pass" name="pass" required>
+    </div>
+    <div>
+      <label for="confirm">Confirm: </label>
+      <input v-model="confirm" @input="validateConfirm" type="password" id="confirm" name="confirm" required>
+    </div>
+    <div class="buttons">
+      <input type="submit" value="submit">
+      <input @click="islogin=true" type="button" value="login">
+    </div>
+  </form> 
+</template>
+
+<script>
+import { mapState, useStore } from 'vuex'
+import { ref } from 'vue'
+
+export default {
+  computed: {
+    ...mapState(['user'])
+  },
+  setup() {
+    const store = useStore()
+    const {dispatch} = store
+    var islogin=ref(true)
+    var username = ref('')
+    var password = ref('')
+    var confirm = ref('')
+
+    const validateConfirm = () => {
+      let pass =document.getElementById('pass')
+      let conf =document.getElementById('confirm')
+      if (pass.value !== conf.value)
+        conf.setCustomValidity('Passwords dont match ... ')
+      else 
+        conf.setCustomValidity('')
+    }
+  
+    const login = () => {
+      let user = {
+        username: username.value,
+        password: password.value
+      }
+      dispatch('login', user)
+    }
+
+    const register = () => {
+      let user = {
+        username: username.value,
+        password: password.value
+      }
+      dispatch('register', user)
+    }
+    return {
+      validateConfirm,
+      islogin,
+      login,
+      register,
+      username,
+      password,
+      confirm,
+    }
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: table;
+  }
+  form>div {
+    display: table-row;
+  }
+  form>div>* {
+    display: table-cell;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import { createStore } from 'vuex'
 
+let state = {}
+let user = localStorage.getItem('user')
+if (user) state={user}
+
 export default createStore({
-  state: {
-  },
+  state,
   mutations: {
   },
   actions: {
+    login (_, user) {
+      console.log(user)
+    }
+    register (_, user) {
+      
+    }
   },
   modules: {
   }
